f3848a62e4f193f38b75e2dbd87ec99af1d53fe7
taochensyd <taochensyd@gmail.com>
2022-11-20 23:59:20 +0000

    Add more validate rules

--- controllers/update.js
+++ controllers/update.js
@@ -1,38 +1,62 @@
 const shell = require("shelljs");
 
+const BekkerToken = "2sGMxTwKeClnILXa3aK2";
+
 const update = (request, response) => {
-  // shell.exec('../ShellScript/updateKubePod.sh')
-
-  console.log(request.body);
-  console.log(request.body.image);
-
-  if ((request.body.image == "atomportal") & (request.body.tag == "develop")) {
-    response.json({
-      kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
-    }); // echo the result back
-    //shell.exec('../ShellScript/updatePortalAPI.sh')
-    shell.exec('microk8s kubectl rollout restart deployment uat-develop-portal-api')
-  }
-
-  if (
-    (request.body.image == "atomportal_frontend") &
-    (request.body.tag == "develop")
+  if (request.body.token !== BekkerToken) {
+    response.status(401).send("Invalid token!");
+  } else if (
+    request.body.token === BekkerToken &&
+    request.body.environment !== null &&
+    request.body.image !== null &&
+    request.body.tag !== null
   ) {
-    response.json({
-      kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
-    }); // echo the result back
-    shell.exec('microk8s kubectl rollout restart deployment uat-develop-portal-frontend')
-  }
+    // shell.exec('../ShellScript/updateKubePod.sh')
 
-  if ((request.body.image == "crmcard_ui") & (request.body.tag == "master")) {
-    response.json({
-      kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
-    }); // echo the result back
-    shell.exec('microk8s kubectl rollout restart deployment uat-master-crmcard-ui')
-  }
+    console.log(request.body);
+    console.log(request.body.image);
 
-  console.log(request.body.tag);
-  console.log(request.body.token);
+    if (
+      (request.body.image.toLowerCase() == "atomportal") &
+      (request.body.tag.toLowerCase() == "develop") &
+      (request.body.environment.toLowerCase() == "uat")
+    ) {
+      response.json({
+        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
+      }); // echo the result back
+      //shell.exec('../ShellScript/updatePortalAPI.sh')
+      shell.exec(
+        "microk8s kubectl rollout restart deployment uat-develop-portal-api"
+      );
+    } else if (
+      (request.body.image.toLowerCase() == "atomportal_frontend") &
+      (request.body.tag.toLowerCase() == "develop") &
+      (request.body.environment.toLowerCase() == "uat")
+    ) {
+      response.json({
+        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
+      }); // echo the result back
+      shell.exec(
+        "microk8s kubectl rollout restart deployment uat-develop-portal-frontend"
+      );
+    } else if (
+      (request.body.image.toLowerCase() == "crmcard_ui") &
+      (request.body.tag.toLowerCase() == "master") &
+      (request.body.environment.toLowerCase() == "uat")
+    ) {
+      response.json({
+        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
+      }); // echo the result back
+      shell.exec(
+        "microk8s kubectl rollout restart deployment uat-master-crmcard-ui"
+      );
+    } else {
+      response.status(400).send("Invalid Parameters");
+    }
+
+    console.log(request.body.tag);
+    console.log(request.body.token);
+  }
 };
 
 module.exports = {
