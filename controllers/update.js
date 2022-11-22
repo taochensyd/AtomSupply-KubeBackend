4066b7d0ada900aac827e903b147b623f9ad722d
taochensyd <taochensyd@gmail.com>
2022-11-23 06:08:24 +0800

    add a timestamp in console.log when updating

--- controllers/update.js
+++ controllers/update.js
@@ -1,4 +1,5 @@
 const shell = require("shelljs");
+const fs = require("fs");
 
 const BekkerToken = "2sGMxTwKeClnILXa3aK2";
 
@@ -17,51 +18,64 @@ const update = (request, response) => {
   ) {
     // shell.exec('../ShellScript/updateKubePod.sh')
 
-    console.log(request.body);
-    console.log(request.body.image);
+    // console.log(request.body);
+    // console.log(request.body.image);
 
     if (
       (request.body.image.toLowerCase() == "atomportal") &
       (request.body.tag.toLowerCase() == "develop") &
       (request.body.environment.toLowerCase() == "uat")
     ) {
+      shell.exec(
+        "microk8s kubectl rollout restart deployment uat-develop-portal-api"
+      );
+      getTimestamp(request.body);
       response.json({
         kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
       }); // echo the result back
       //shell.exec('../ShellScript/updatePortalAPI.sh')
-      shell.exec(
-        "microk8s kubectl rollout restart deployment uat-develop-portal-api"
-      );
     } else if (
       (request.body.image.toLowerCase() == "atomportal_frontend") &
       (request.body.tag.toLowerCase() == "develop") &
       (request.body.environment.toLowerCase() == "uat")
     ) {
-      response.json({
-        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
-      }); // echo the result back
       shell.exec(
         "microk8s kubectl rollout restart deployment uat-develop-portal-frontend"
       );
+      response.json({
+        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
+      }); // echo the result back
     } else if (
       (request.body.image.toLowerCase() == "crmcard_ui") &
       (request.body.tag.toLowerCase() == "master") &
       (request.body.environment.toLowerCase() == "uat")
     ) {
-      response.json({
-        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
-      }); // echo the result back
       shell.exec(
         "microk8s kubectl rollout restart deployment uat-master-crmcard-ui"
       );
+      response.json({
+        kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
+      }); // echo the result back
     } else {
       response.status(400).send("Invalid Parameters");
     }
 
-    console.log(request.body.tag);
-    console.log(request.body.token);
+    // console.log(request.body.tag);
+    // console.log(request.body.token);
   }
 };
+
+// Log the timestamp to local log.txt file.
+
+async function getTimestamp(postParams) {
+  try {
+    // let currentTimestamp = await shell.exec("date");
+    console.log(`${shell.exec("date")}${postParams.environment}:${postParams.image}:${postParams.tag}`);
+    // await fs.appendFileSync("updateLog.txt", `${shell.exec("date")}${postParams}`);
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 module.exports = {
   update,
