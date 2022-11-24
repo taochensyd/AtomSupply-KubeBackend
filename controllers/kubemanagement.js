20180e53d26adf346c61db621a2618065acfe568
Tao Chen <taochensyd@gmail.com>
2022-11-25 08:11:21 +1100

    add a fetch request

--- controllers/kubemanagement.js
+++ controllers/kubemanagement.js
@@ -1,10 +1,14 @@
 const shell = require("shelljs");
-const webToken = "fjhsiufoefoefoifeoi";
+// import fetch from ("node-fetch");
+const webToken = "Web-User";
 
-const kubePorts = [31000, 31001, 31002, 30000, 30001, 30003, 30011, 30012, 31011, 31012, 32011, 32012];
+const kubePorts = [
+  31000, 31001, 31002, 30000, 30001, 30003, 30011, 30012, 31011, 31012, 32011,
+  32012,
+];
 
 const kubemanagement = (request, response) => {
-    /*
+  /*
         request body:
         {
             portID:31000,
@@ -14,32 +18,38 @@ const kubemanagement = (request, response) => {
     
     */
 
-    
-
-  if (request.body.token !== "fjhsiufoefoefoifeoi") {
+  if (request.body.token !== "Web-User") {
     response.status(400).send("Invalid Token");
-  } else if (request.body.token === "fjhsiufoefoefoifeoi") {
-    
-    
-    fetch('https://kube-api-endpoint.atom.com.au/update', {
-        Method: 'POST',
-        Headers: {
-          Accept: 'application.json',
-          'Content-Type': 'application/json'
-        },
-        Body: body,
-        Cache: 'default'
-      })
-    
-    response.status(200).send("web management controller.");
-
-
-
+  } else if (request.body.token === "Web-User") {
+    // response.status(200).send("web management controller.");
+    makePostUpdateRequest({
+      token: "Web-User",
+      environment: "uat",
+      image: "atomportal_frontend",
+      tag: "develop",
+    });
   } else {
     response.status(400).send("Invalid Parameters");
   }
 };
 
+// function makePostUpdateRequest(body) {
+//   fetch("https://kube-api-endpoint.atom.com.au/update", {
+//     Method: "POST",
+//     Headers: {
+//       Accept: "application.json",
+//       "Content-Type": "application/json",
+//     },
+//     Body: body,
+//     Cache: "default",
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       return data;
+//     });
+// }
+
 module.exports = {
   kubemanagement,
 };
+
