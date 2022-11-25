49d10affc7296b438b2160213d80b12df96485aa
taochensyd <taochensyd@gmail.com>
2022-11-25 12:14:18 +0800

    addd email respond

--- controllers/update.js
+++ controllers/update.js
@@ -1,7 +1,26 @@
 const shell = require("shelljs");
+const nodemailer = require("nodemailer");
 const BekkerToken = "2sGMxTwKeClnILXa3aK2";
 
 const update = (request, response) => {
+  //Email Section
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "sydneyau01@gmail.com",
+      pass: "Mimashi123",
+    },
+  });
+
+  var mailOptions = {
+    from: "sydneyau01@gmail.com",
+    to: "tao.chen@atom.com.au",
+    subject: "automatic email from kube backend",
+    text: "text body",
+  };
+
+  //End Email Section
+
   if (request.body.token !== BekkerToken) {
     response.status(401).send("Invalid token!");
   } else if (
@@ -41,6 +60,13 @@ const update = (request, response) => {
       shell.exec(
         "microk8s kubectl rollout restart deployment uat-develop-portal-frontend"
       );
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
       response.json({
         kubeUpdate: `${request.body.image}:${request.body.tag} has been successfully updated`,
       }); // echo the result back
@@ -69,10 +95,14 @@ const update = (request, response) => {
 async function getTimestamp(postParams) {
   try {
     // let currentTimestamp = await shell.exec("date");
-    console.log(`${shell.exec("date")}${postParams.environment}:${postParams.image}:${postParams.tag}`);
+    console.log(
+      `${shell.exec("date")}${postParams.environment}:${postParams.image}:${
+        postParams.tag
+      }`
+    );
     // await fs.appendFileSync("updateLog.txt", `${shell.exec("date")}${postParams}`);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
--- index.js
+++ index.js
@@ -3,32 +3,57 @@ const app = express();
 const lineReader = require("line-reader");
 const update = require("./routes/update");
 const fs = require("fs");
+<<<<<<< HEAD
 require("dotenv").config();
+=======
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+>>>>>>> d363c8e (add email respond)
 
 //middleware
 app.use(express.json());
 
 //route
+app.get("/", (req, res) => {
+  res.sendFile('views/index.html', {root: __dirname })
+});
+
 app.get("/hello", (req, res) => {
   res.send("NodeJS+Express");
 });
 
 app.get("/api/v1/getOutLog", (req, res) => {
+<<<<<<< HEAD
   fs.readFile("logs/index-out.log", "utf8", (err, data) => {
+=======
+  fs.readFile("../.pm2/logs/index-out.log", "utf8", (err, data) => {
+>>>>>>> d363c8e (add email respond)
     if (err) {
       console.error(err);
       return;
     }
+<<<<<<< HEAD
+=======
+    // sendFile('views/logs.html', {root: __dirname })
+>>>>>>> d363c8e (add email respond)
     res.send(data);
   });
 });
 
 app.get("/api/v1/getErrorLog", (req, res) => {
+<<<<<<< HEAD
   fs.readFile("logs/index-error.log", "utf8", (err, data) => {
+=======
+  fs.readFile("../.pm2/logs/index-error.log", "utf8", (err, data) => {
+>>>>>>> d363c8e (add email respond)
     if (err) {
       console.error(err);
       return;
     }
+<<<<<<< HEAD
+=======
+    // sendFile('views/logs.html', {root: __dirname })
+>>>>>>> d363c8e (add email respond)
     res.send(data);
   });
 });
@@ -37,6 +62,22 @@ app.use("/update", update);
 // app.use("/api/v1/kubemanagement", kubemanagement);
 // app.use("/api/v1/getLogs", getLogs);
 
+<<<<<<< HEAD
+=======
+
+
+//Email Section
+
+
+
+//End Email Section
+
+
+
+
+
+
+>>>>>>> d363c8e (add email respond)
 const port = 3500;
 
 app.listen(port, console.log(`Server is listening on port ${port}`));
